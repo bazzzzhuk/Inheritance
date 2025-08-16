ef4b3086d31e8ede0befdd350f6c2dd4f2ae408b
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-08-17 00:35:38 +0300

    edit

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -5,7 +5,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-#define delimiter "\n--------------------------------------------\n"
+#define delimiter "\n----------------------------------------------\n"
 
 #define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
 #define HUMAN_GIVE_PARAMETERS last_name, first_name, age
